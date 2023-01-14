02a5360cc2f1a663b7e2dee6e29b83365edc1800
Roman Bortnikow <rb2o2.dev@gmail.com>
2023-01-14 23:23:51 +0300

    + added test for recipe database consistency

---
+++ src/test/scala/ru/pangaia/cookingallbot/DBIntegrityTest.scala
@@ -0,0 +1,23 @@
+package ru.pangaia.cookingallbot
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class DBIntegrityTest extends AnyFlatSpec {
+  val fullIndex: Index = Index("data.zip", true)
+
+  "all recipes" should "have parseable cookTimes" in {
+    try {
+      println(fullIndex.data.sorted(Recipe.timeOrdering.reverse)(1))
+    } catch {
+      case x: Exception => fail()
+    }
+  }
+
+  "all recipes" should "have nonempty ingredients list" in {
+    assert(!fullIndex.data.exists(_.ingredients.isEmpty))
+  }
+
+  "all recipes" should "have nonempty instruction list" in {
+    assert(!fullIndex.data.exists(_.steps.isEmpty))
+  }
+}
